534f7f2728e2f4d135e9cab52d2cf2a06cf3ae62
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-02-29 12:06:35 -0600

    start A1

---
+++ A1 character/sketch.js
@@ -0,0 +1,23 @@
+// A1 character
+// matthew
+// 2/29/feb 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill('yellow')
+  circle(width/2, height/2,100)
+  rect(340,500,100,200)
+  rect(287,500,50,150)
+  rect(443,500,50,150)
+  rect(400,705,40,150)
+  rect(340,705,40,150)
+  
+}
