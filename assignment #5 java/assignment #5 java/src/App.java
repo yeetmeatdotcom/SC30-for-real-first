12f61e2293f700da1f59673c858973d98e17b058
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-03-27 11:35:38 -0600

    homework

---
+++ assignment #5 java/assignment #5 java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+//Welcome to Computer Science 30 My Name is (insert your name) and my favourite food is (insert food). Java has all sorta of weird "quirks" which make displaying text a bit more challenging. Sometimes characters like parentheses (sometimes called brackets) don't work correctly. Escape sequences require us to use a backslash '\' in order to output these special characters.
