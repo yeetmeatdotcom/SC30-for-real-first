e91074597fbfb5610c6f15564e2b17c621ee37a0
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-03-27 12:31:54 -0600

    done 6

--- assignment # 6 java/assignment # 6 java/src/App.java
+++ assignment # 6 java/assignment # 6 java/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double cash = 4.75;
+        int wings = (int) (cash / 0.75);
+
+        System.out.println("I can buy " + wings + " of this wings");
     }
 }
