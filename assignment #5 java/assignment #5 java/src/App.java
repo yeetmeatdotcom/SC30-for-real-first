02fa28a2457efea3e2ef066c775686fedeceb5f3
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-03-27 12:23:25 -0600

    assignment 5 done

--- assignment #5 java/assignment #5 java/src/App.java
+++ assignment #5 java/assignment #5 java/src/App.java
@@ -1,6 +1,16 @@
+//Program/Assignment name: (The name you gave the program)
+//Student name: (Your name)
+//Date: (The date you completed the program)
+//Program Description:
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Computer Science 30");
+        System.out.println("My Name is (insert your name) and my favourite food is (insert food).");
+        System.out.println("Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.");
+        System.out.println("Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
+        System.out.println("Escape sequences require us to use a backslash '\\' in order to output these special characters.\n" + //
+                        "");
     }
 }
 //Welcome to Computer Science 30 My Name is (insert your name) and my favourite food is (insert food). Java has all sorta of weird "quirks" which make displaying text a bit more challenging. Sometimes characters like parentheses (sometimes called brackets) don't work correctly. Escape sequences require us to use a backslash '\' in order to output these special characters.
