3bc26a9ecda111315f0638521e6cb8a88aaa035d
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-03-28 11:43:51 -0600

    Update App.java

--- test java/test java/src/App.java
+++ test java/test java/src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("name matthew!");
-
-    }
+     String s1 = "Hi";
+     String s2 = "Bye";
+     int answer = s1.compareTo(s2);
 }
