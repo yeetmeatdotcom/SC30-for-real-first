0a237e8ca8be589e17e4144c63aa3112fdcfb87f
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-04-08 11:20:39 -0600

    conditionaldemo

---
+++ conditionaldemo/conditionaldemo/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        int x = -13;
+
+        // This conditiom check if x over 5
+        if (x>5){
+            System.out.println("BIG NUMBER");}
+            // this is -12 < x < 5 because 5 ready checked
+            else if (x > -12) {
+                System.out.println("Mild weather");
+            }
+            // this is x =< -12 becasue the others have been checkd 
+            else {
+                System.out.println("very cold");
+
+            }
+        
+    
+             boolean isCold = false;
+
+               if (!isCold) {
+               System.out.println("war a coat");
+             System.out.println("war a Gloves");
+        }
+    }
+}
+
+
