f6e1913d128ca9361a1eec1df95cc93379d7b921
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-04-11 11:25:45 -0600

    assignment 9 not done

--- assignment #9 java big or small/#9 big or small/src/App.java
+++ assignment #9 java big or small/#9 big or small/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+		BigOrSmall.check( 10, 20);		
+		BigOrSmall.check( 20, 10);	
+		BigOrSmall.check( 20, 20);
+		BigOrSmall.check( 10, 10);
+
+		BigOrSmall.check( 0, 1);	
+		BigOrSmall.check( 1, 0 );
+        	
+		BigOrSmall.check( 3, 5);	
+		BigOrSmall.check( 5, 3 );	
+		BigOrSmall.check( 5342, 323 );	
     }
 }
--- assignment #9 java big or small/#9 big or small/src/BigOrSmall.java
+++ assignment #9 java big or small/#9 big or small/src/BigOrSmall.java
@@ -1,15 +1,21 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
-import static java.lang.System.*; 
-import java.util.Scanner;
-
-public class BigOrSmall
-{
-	public static void check( int a, int b )
+public class BigOrSmall {
+    public static void check( int a, int b )
 	{
-		
-		System.out.println() //Answer
-	}
+        if (a < b) System.out.println("no");
+    {
+        if (b > a) System.out.println("yes");
+    }{
+        if (b == b) System.out.println("aplus");
+    }{
+        if (a == a) System.out.println("aplus");
+	}{
+        if (a < b) System.out.println("no");
+    }{
+        if (b > a) System.out.println("yes");
+    }{
+        if (a < b) System.out.println("no");
+    }{
+        if (b > a) System.out.println("yes");
+    }
+}
 }
