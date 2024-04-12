ca1915ad5063840e9b0c1d1d47681469113b1175
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-04-12 11:25:05 -0600

    done 9

--- assignment #9 java big or small/#9 big or small/src/BigOrSmall.java
+++ assignment #9 java big or small/#9 big or small/src/BigOrSmall.java
@@ -1,21 +1,13 @@
 public class BigOrSmall {
     public static void check( int a, int b )
 	{
-        if (a < b) System.out.println("no");
-    {
-        if (b > a) System.out.println("yes");
-    }{
-        if (b == b) System.out.println("aplus");
-    }{
-        if (a == a) System.out.println("aplus");
-	}{
-        if (a < b) System.out.println("no");
-    }{
-        if (b > a) System.out.println("yes");
-    }{
-        if (a < b) System.out.println("no");
-    }{
-        if (b > a) System.out.println("yes");
+        if (b < a) System.out.println("yes");
+    
+        else if (b > a) System.out.println("no");
+
+        else{
+         System.out.println("aplus");
+
+        }
     }
-}
-}
+    }
