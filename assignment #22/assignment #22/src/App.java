9e9b41d3d887c7a1ccb6963373b12d933ef838f5
yeetmeatdotcom <matthew.de-paz@rcsd.ca>
2024-04-29 12:06:42 -0600

    done

---
+++ assignment #22/assignment #22/src/App.java
@@ -0,0 +1,13 @@
+
+
+
+public class App {
+	public static boolean isLeapYear( int year )
+	{
+        if ((year % 4 == 0 && year % 100 != 0)|| (year % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+	}
+}
---
+++ assignment #22/assignment #22/src/LeapyearRunner.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class LeapyearRunner {
+    public static void main( String args[] )
+	{
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Enter a year = ");
+        int year = keyboard.nextInt();
+        
+        if (App.isLeapYear(year)) 
+        {
+            System.out.println(year + " A leap year.");
+        } 
+        else 
+        {
+            System.out.println(year + " Not a leap year.");
+        }
+        keyboard.close();
+	}
+}
